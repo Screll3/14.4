8a846d5eac2489f5f901a5c1e1cb0c8f613dbab4
Ian <80196814+Screll3@users.noreply.github.com>
2022-06-29 14:24:45 +0700

    Home Work
    14.4

---
+++ 14.4/14.4.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string s;
+    std::cout << "Enter string: ";
+    std::getline(std::cin, s);
+    std::cout << "String: " << s << "\nString size: " << s.length() << "\nFirsc char: " << s[0] << "\nLastChar: " << s[s.length() - 1];
+}
