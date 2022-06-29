b1c5f635a5a3693b4246f7ec52bec900a3836adc
Ian <80196814+Screll3@users.noreply.github.com>
2022-06-29 16:34:53 +0700

    Update 14.4.cpp
    Use functions

--- 14.4/14.4.cpp
+++ 14.4/14.4.cpp
@@ -6,5 +6,5 @@ int main()
     std::string s;
     std::cout << "Enter string: ";
     std::getline(std::cin, s);
-    std::cout << "String: " << s << "\nString size: " << s.length() << "\nFirsc char: " << s[0] << "\nLastChar: " << s[s.length() - 1];
+    std::cout << "String: " << s << "\nString size: " << s.length() << "\nFirst char: " << s.front() << "\nLastChar: " << s.back();
 }
