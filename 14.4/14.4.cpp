00315cd0e0f6b65eda0068935faeec6c8790dcca
Ian <80196814+Screll3@users.noreply.github.com>
2022-06-29 16:36:44 +0700

    Update 14.4.cpp

--- 14.4/14.4.cpp
+++ 14.4/14.4.cpp
@@ -6,5 +6,5 @@ int main()
     std::string s;
     std::cout << "Enter string: ";
     std::getline(std::cin, s);
-    std::cout << "String: " << s << "\nString size: " << s.length() << "\nFirst char: " << s.front() << "\nLastChar: " << s.back();
+    std::cout << "String: " << s << "\nString size: " << s.length() << "\nFirst char: " << s.front() << "\nLast Char: " << s.back();
 }
